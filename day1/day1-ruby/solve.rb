f882b26256fc8bef9552b340c415b0c626806884
Simão Belchior <simao@platforme.com>
2024-12-04 15:03:01 +0000

    Small updates to day 1 ruby and gitignore

---
+++ day1/day1-ruby/solve.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+col1 = []
+col2 = []
+
+File.open("../input.txt", "r").each do |line|
+  first, second = line.split(" ")
+  col1 << first.to_i
+  col2 << second.to_i
+end
+
+if col1.size != col2.size
+  puts "Columns are not the same size..."
+  return
+end
+
+col1.sort!
+col2.sort!
+
+differences = 0
+
+(0...col1.size).each do |i|
+  differences += (col1[i] - col2[i]).abs
+end
+
+puts "Total difference is: #{differences}"
